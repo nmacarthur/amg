8d307bf8fcc54794d2bfd46c2b558725afa1fc96
Nicholas MacArthur <n.j.macarthur@gmail.com>
2018-08-16 13:32:44 +1000

    s

--- footer.php
+++ footer.php
@@ -192,7 +192,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	});
 	
 	function toggleModal(id){ 
-		var modal = document.getElementById(id);
+		var modal = document.getElementById(id); 
 		jQuery('#'+id).modal('toggle');
 	} 
 
